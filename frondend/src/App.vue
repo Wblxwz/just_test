aee24e7ddffbeaaae69863a8c6aa440fa4515c7a
wblxwz <2394559659@qq.com>
2026-02-13 21:15:52 +0800

    init

---
+++ frondend/src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+// 获取当前路由信息
+const route = useRoute();
+</script>
+
+<template>
+  <div v-if="route.name !== 'Login'" class="common-layout">
+    <el-container style="height:100%">
+      <el-header><h1 style="margin-top: 0.1%;">Just-Test</h1></el-header>
+      <el-main><router-view/></el-main>
+      <el-footer><h2 style="margin-top: 0.1%;">评论区</h2></el-footer>
+    </el-container>
+  </div>
+  <router-view v-else/>
+</template>
+
+<style>
+.common-layout{
+  height: 100vh;
+}
+
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+
+</style>
